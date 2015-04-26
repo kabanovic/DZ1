7649a3c0ac4a345fcde9bc1138483d9052d6e7ad
Аким <akim@Mac-mini-Akim.local>
2015-04-26 17:41:19 +0300

    Initial Commit

---
+++ DZ1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DZ1
+//
+//  Created by Аким on 26.04.15.
+//  Copyright (c) 2015 Akim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
